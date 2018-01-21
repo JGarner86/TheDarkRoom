3474fe8c0e2de62753700e09a788be0032d3395f
sicke <sicke@DESKTOP-2I59MVV>
2018-01-21 14:00:38 -0600

    trying my self

---
+++ DarkRoomProject/BobbysTestLib/gamedialog2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BobbysTestLib
+{
+    class gamedialog2    
+    {
+        public static string  
+
+    }
+}
