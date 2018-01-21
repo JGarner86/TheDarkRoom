b52b340ad013c2d5f67a1b2eb2dced2df586a66f
JGarner86 <garnerj2005@gmail.com>
2018-01-21 15:52:18 -0600

    Revert "got it"
    This reverts commit 1205a16c1610a920d42321984be9324f7106debe.

--- DarkRoomProject/BobbysTestLib/TestModule.cs
+++ DarkRoomProject/BobbysTestLib/TestModule.cs
@@ -14,12 +14,45 @@ namespace BobbysTestLib
 
         public static void RunTest()
         {
-            Test(SimpleMath.AddTwoNumbers(4, 3), 7, "AddTwoNumbers");
-            Test(SimpleMath.MinusTwoNumbers(10, 5), 5, "MinusTwoNumbers");
-            Test(SimpleMath.TimesTwoNumbers(6, 3), 18, "TimesTwoNumbers");
-            Test(SimpleMath.DivideTwoNumbers(10, 100), 10, "DivideTwoNumbers");
-            Test(SimpleMath.CheckIfTwoNumbersAreEqual(5, 5), true, "CheckIfTwoNumbersAreEqual");
+            //Test(SimpleMath.AddTwoNumbers(4, 3), 7, "AddTwoNumbers");
+            //Test(SimpleMath.MinusTwoNumbers(10, 5), 5, "MinusTwoNumbers");
+            //Test(SimpleMath.TimesTwoNumbers(6, 3), 18, "TimesTwoNumbers");
+            //Test(SimpleMath.DivideTwoNumbers(10, 100), 10, "DivideTwoNumbers");
+            //Test(SimpleMath.CheckIfTwoNumbersAreEqual(5, 5), true, "CheckIfTwoNumbersAreEqual");
             
+        }
+
+        public static void RunTest(Person personA, Person personB)
+        {
+            Person Bobby = new Person
+            {
+                FirstName = "Bobby",
+                LastName = "Brooks",
+                City = "Midland",
+                State = "Tx",
+                Street = "354 Midkiff",
+                Zip = "79705",
+                PhoneNumber = "432-555-2365",
+                
+            };
+
+            Person Josh = new Person
+            {
+                FirstName = "Josh",
+                LastName = "Garner",
+                City = "Cedar Park",
+                State = "Tx",
+                Street = "610 Cheyenne Ln",
+                Zip = "78613",
+                PhoneNumber = "737-212-4225",
+
+            };
+
+            Bobby.CallPerson(Josh);
+            Bobby.PrintAddress();
+
+
+
         }
 
         private static void Test(int method, int expectedResult, string name)
