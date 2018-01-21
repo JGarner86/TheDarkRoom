d7e2420b020e7df98f7977403a9e2bc70ee2f818
sicke <sicke@DESKTOP-2I59MVV>
2018-01-21 14:40:46 -0600

    Got it bro

--- DarkRoomProject/BobbysTestLib/SimpleMath.cs
+++ DarkRoomProject/BobbysTestLib/SimpleMath.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,33 +9,42 @@ namespace BobbysTestLib
     public class SimpleMath
     {
         // Make this add method work by letting the caller pass in two arguments and you return the result of adding the two arguments together.
-        public static int AddTwoNumbers()
+        public static int AddTwoNumbers(int number1, int number2)
         {          
-            return 0;
+            return number1 + number2;
         }
 
         // Make this minus method work by letting the caller pass in two arguments and you return the result of minusing one argument from the other.
-        public static int MinusTwoNumbers()
+        public static int MinusTwoNumbers(int number4, int number2)
         {
-            return 0;
+            return number4 - number2;
         }
 
         // Make this times method work by letting the caller pass in two arguments and you return the result of multiplying the two arguments together.
-        public static int TimesTwoNumbers()
+        public static int TimesTwoNumbers(int number5, int number3)
         {
-            return 0;
+            return number5 * number3;
         }
 
         // Make this divide method work by letting the caller pass in two arguments and you return the result of dividing one argument from the other.
-        public static int DivideTwoNumbers()
+        public static int DivideTwoNumbers(int number6, int number9)
         {
-            return 0;
+            return number6 / number9;
         }
 
         // Make this equals method work by letting the caller pass in two arguments and you return a true if they are equal, false if they are not.
-        public static bool CheckIfTwoNumbersAreEqual()
+        public static bool CheckIfTwoNumbersAreEqual(int number7, int number8)
         {
-            return false;
+            if (number7 == number8)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+           
         }
-    }
+    }   
 }
