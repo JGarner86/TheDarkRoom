ca40ab3258e241054bf550286dceab93ef38cd5a
JGarner86 <garnerj2005@gmail.com>
2018-01-23 02:30:30 -0600

    Made a UserController

---
+++ DarkRoomProject/BobbysTestLib/UserController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BobbysTestLib
+{
+    public class UserController
+    {
+        public static ConsoleKey UserKeyPress { get{ return getKeyPress(); } }
+
+        private static List<ConsoleKey> keyMap = new List<ConsoleKey> { ConsoleKey.R, ConsoleKey.H, ConsoleKey.F };
+        //private static ConsoleKey userKey = getKeyPress();
+        private static ConsoleKey getKeyPress()
+        {
+
+            ConsoleKey userKey = Console.ReadKey().Key;
+            if (!keyMap.Contains(userKey))
+            {
+                Console.WriteLine("You have entered an invalid key...");
+                
+                getKeyPress();
+               
+            }
+            return userKey;
+        }
+    }
+}
