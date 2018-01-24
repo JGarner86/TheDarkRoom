ec663e746edeedeac9b190f509a3cdbd4dfbf7ed
sicke <sicke@DESKTOP-2I59MVV>
2018-01-24 00:19:21 -0600

    made changes

--- darkroomproject/bobbystestlib/player.cs
+++ darkroomproject/bobbystestlib/player.cs
@@ -7,22 +7,28 @@ using System.Threading.Tasks;
 namespace BobbysTestLib
 {
     public class Player : Character
-    {
-       public void SwordSlash(Player player, Bear bear)
+    { /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="character"></param>
+       public void SwordSlash(Character character)
        {
             int attackAmount = -40;
-            DamageAdjust(ref attackAmount);
-            bear.Health += attackAmount;
-            Console.WriteLine($"{player.Name} Hit {bear.Name} with swordSlash for{attackAmount}damage");
+            StaminaDamageAdjust(ref attackAmount, AttackType.Medium);
+           character.Health += attackAmount;
+            Console.WriteLine($"{Name} Hit {character.Name} with swordSlash for{attackAmount}damage");
        }
 
-
-       public void HeavyAttack(Player player, Bear bear)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="character"></param>
+       public void HeavyAttack(Character character)
        {
             int attackAmount = -80;
-            DamageAdjust(ref attackAmount);
-            bear.Health += attackAmount;
-            Console.WriteLine($"{player.Name} Hit {bear.Name} with a Heavy attack for{attackAmount}");
+            StaminaDamageAdjust(ref attackAmount, AttackType.Heavy);
+            Health += attackAmount;
+            Console.WriteLine($"{character.Name} Hit {Name} with a Heavy attack for{attackAmount}");
        }
 
 
