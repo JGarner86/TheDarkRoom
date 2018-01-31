86368d75c289b48749467b79dd7898852d43adf4
JGarner86 <garnerj2005@gmail.com>
2018-01-30 23:47:20 -0600

    Made some strides in learning delegates and events

---
+++ DarkRoomProject/BobbysTestLib/Animation/AnimationTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BobbysTestLib.Animation
+{
+    public class AnimationTest
+    {
+
+        /// <summary>
+        /// Place to run test animation code
+        /// </summary>
+        public static void RunCode()
+        {
+
+        }
+
+
+        private char leftArmRaised = '└';
+        private char armExtended = '─';
+        private char rightArmRaised = '┘';
+        private char leftLegBent = '┌';
+        private char rightLegBent = '┐';
+        private char torso = '╬';
+        private char hips = '▬';
+        private char head = '☺';
+
+
+    }
+}
